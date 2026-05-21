c7e613d3fccbca8d315bcb0baf7c2ce397a4e14b
kashyapahir <kashyapahir786@gmail.com>
2026-05-22 01:04:11 +0530

    added trust proxy for cookies

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,9 @@ const followRoute = require("./routes/follow.routes")
 const cors = require("cors")
 
 const app = express()
+
+app.set("trust proxy", 1);
+
 app.use(cookieParser())
 app.use(express.json())
 
